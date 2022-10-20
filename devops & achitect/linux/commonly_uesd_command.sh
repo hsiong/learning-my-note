55de5e9e49985e2463764909f980c7366dc95977
hsiong <peak1203@126.com>
2022-10-20 10:01:32 +0800

    update shell note

--- devops & achitect/linux/commonly_uesd_command.sh
+++ devops & achitect/linux/commonly_uesd_command.sh
@@ -1,7 +1,7 @@
 # kill process by name
 ps aux|grep $name|grep -v grep|awk '{print $2}'|xargs kill
 # kill process by port
-ps aux|grep $port|grep -v grep|awk '{if(NR>=1) print $2}'|xargs kill
+lsof -i:$port|awk '{if(NR>=2) print $2}'|xargs kill
 
 # start java
 nohup java -jar  /root/backend/url-shortcut/project-URL-shortcut.jar -Ddebug -Dname=/root/backend/url-shortcut/project-URL-shortcut.jar -Xms64m -Xmx512m -Xmn256m  -Xss256k --add-opens java.base/java.lang=ALL-UNNAMED --spring.profiles.active=prod > out.file 2>&1 & \
