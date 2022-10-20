377466edfe478a5ae492effcc5deb1197cc78dda
hsiong <peak1203@126.com>
2022-10-20 10:09:05 +0800

    update shell note

--- devops & achitect/linux/commonly_uesd_command.sh
+++ devops & achitect/linux/commonly_uesd_command.sh
@@ -7,3 +7,12 @@ lsof -i:$port|awk '{if(NR>=2) print $2}'|xargs kill
 nohup java -jar  /root/backend/url-shortcut/project-URL-shortcut.jar -Ddebug -Dname=/root/backend/url-shortcut/project-URL-shortcut.jar -Xms64m -Xmx512m -Xmn256m  -Xss256k --add-opens java.base/java.lang=ALL-UNNAMED --spring.profiles.active=prod > out.file 2>&1 & \
 tail -500f out.file
 
+# connect redis by telnet 
+telnet localhost 6379   
+auth 'pwd'
+key *
+## quit telnet
+exit 
+^]
+q
+
