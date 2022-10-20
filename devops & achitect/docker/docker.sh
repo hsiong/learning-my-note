4dba7fbdcca452d50ffa5ef61866dfa7afcd5a1a
hsiong <peak1203@126.com>
2022-10-20 14:14:20 +0800

    update nginx note

--- devops & achitect/docker/docker.sh
+++ devops & achitect/docker/docker.sh
@@ -17,6 +17,19 @@ docker info
 ## 
 
 ## docker - nginx
+# 注意: 该配置容器只会监听 80 端口, 所以其他端口做不了反向代理
+# server {
+#     listen 7001;
+#     server_name  localhost;
+#     location /test {
+#         proxy_set_header Host $host;   #nginx的变量$host，代表实际的host
+#         proxy_set_header X-Real-IP $remote_addr;  #nginx的变量$host，代表实际的address
+#         # nginx的变量$host代表实际的主机，$request_uri代表实际的请求链接，也可以用$args代替
+#         # 注意: $request_uri 前如果有个/的话, 匹配的 xxx//xxx 那么自然找不到了
+#         proxy_pass  http://106.13.203.73:8001/url-shortcut/redirect$request_uri; 
+#     }
+# }
+
 docker run --name nginx -p 80:80 \
 -v /root/config/docker/:/etc/nginx/conf.d/ \
 -v /root/download:/usr/share/nginx/download \
