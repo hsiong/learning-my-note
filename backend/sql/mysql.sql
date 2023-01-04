602f54866037c5b1787571ca81006007a0a80ae7
hsiong <peak1203@126.com>
2023-01-04 18:02:05 +0800

    add mysql note

---
+++ backend/sql/mysql.sql
@@ -0,0 +1,7 @@
+## utf8 vs utf8mb4
+https://juejin.cn/post/6844903733034221576
+
+## create DATABASE
+CREATE DATABASE `mydb` CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE  `wordpress` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
