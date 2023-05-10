4fc931b10ec5823f8bf83f289ca62d9f7517f8b3
hsiong <peak1203@126.com>
2023-05-10 18:23:26 +0800

    upgrade nginx note

--- devops & achitect/docker/docker-nginx/nginx.sh
+++ devops & achitect/docker/docker-nginx/nginx.sh
@@ -25,3 +25,12 @@ return 301 https://$host$request_uri;
 
 # 主域名跳转至带 www 的二级域名
 修改主域名解析配置为 A 记录，记录值直接指向服务器IP。
+
+# 古早项目, nginx托管静态资源跨域问题处理 https://www.77nn.net/2216.html
+在资源设置文件中, 添加 
+```
+    add_header Access-Control-Allow-Origin *;
+    add_header Access-Control-Allow-Headers X-Requested-With;
+    add_header Access-Control-Allow-Methods GET,POST,OPTIONS;
+```
+
