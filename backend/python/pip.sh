c133764c7e96509d71c8f2b2fed498ef5cee7885
hsiong <peak1203@126.com>
2023-11-03 13:52:47 +0800

    upgrade pip note

---
+++ backend/python/pip.sh
@@ -0,0 +1,24 @@
+
+# pip 安装依赖
+pip install -r requirements.txt
+
+> 注意: 国外源会出现很多问题
+
+## 国内源
+https://zhuanlan.zhihu.com/p/623325525?utm_id=0&wd=&eqid=
+
+## 默认源
+https://pypi.org/simple
+
+## 修改默认源为清华源
+https://mirrors.tuna.tsinghua.edu.cn/help/pypi/
+
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+
+## ChatGLM-6B-0001-环境准备
+https://zhuanlan.zhihu.com/p/647859484
+
+## NCCL windows 安装失败
+NCCL 是 Nvidia 为 linux 多显卡实现的标准, 无 windows 版本
+http://www.360doc.com/content/12/0121/07/77158047_1083248145.shtml
+
