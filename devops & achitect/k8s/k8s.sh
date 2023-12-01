dba44dfe5215845ce2c1ffef4b38416611965af3
hsiong <peak1203@126.com>
2023-12-01 15:40:27 +0800

    upgrade k8s note

--- devops & achitect/k8s/k8s.sh
+++ devops & achitect/k8s/k8s.sh
@@ -24,3 +24,37 @@ kubectl describe pod <pod-name> -n <namespace>
 # 检查Kubernetes Pod的Spec
 kubectl get pod <pod-name> -o yaml
 
+
+
+# 一些问题
+> kubectl get pod, No resources found in default namespace.
+如果您在使用 kubectl get pod 命令时收到了 “No resources found in default namespace.” 的消息，这意味着在默认命名空间中没有正在运行的Pod。
+
++ 确保Kubernetes集群状态良好
+kubectl cluster-info
+
++ 检查特定命名空间：
+kubectl get pods -n <namespace>
+
+> k8s 容器运行额外命令
+```
+apiVersion: v1
+kind: Pod
+metadata:
+  name: example-pod
+spec:
+  containers:
+  - name: example-container
+    image: your-image
+    command: ["/bin/sh"]
+    args: ["-c", "echo Hello, Kubernetes! && exec your-original-command"]
+```
+
+> k8s 如何查看原有的入口点(ENTRYPOINT)
+docker inspect xxx 
+
+
+
+
+
+
