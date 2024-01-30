6d56edfe30e50194ef3f74c5c639d007300cd0f8
hsiong <peak1203@126.com>
2024-01-30 09:52:14 +0800

    upgrade k8s note

--- devops & achitect/k8s/k8s.sh
+++ devops & achitect/k8s/k8s.sh
@@ -30,6 +30,19 @@ kubectl get pod <pod-name> -o yaml
 > kubectl get pod, No resources found in default namespace.
 如果您在使用 kubectl get pod 命令时收到了 “No resources found in default namespace.” 的消息，这意味着在默认命名空间中没有正在运行的Pod。
 
+## k8s  imagePullPolicy 有哪些
+Kubernetes (k8s) 中的 imagePullPolicy 用于指定容器镜像的拉取策略。这是一个重要的设置，因为它决定了 Kubernetes 如何从镜像仓库获取镜像。以下是可用的 imagePullPolicy 选项：
+> Always: 总是尝试从远程仓库拉取镜像，不论本地是否已有镜像缓存。这确保了每次都使用最新的镜像，但可能会增加拉取时间和网络流量。
+> Never: 总是使用本地镜像，从不从远程仓库拉取。如果本地没有相应的镜像，容器启动会失败。这种策略适用于已经确信本地镜像存在且不需要更新的情况。
+> IfNotPresent: 如果本地不存在镜像，则从远程仓库拉取。如果本地已有镜像缓存，则使用本地镜像。这是默认的拉取策略，平衡了性能和保证镜像最新的需求。
+选择哪种 imagePullPolicy 取决于你的特定需求。例如，对于生产环境，可能更倾向于使用 Always 策略以确保总是运行最新版本的镜像。而在开发或测试环境中，可能会选择 IfNotPresent 或 Never 以减少拉取镜像的时间和网络带宽消耗。
+
+## k8s readinessProbe
+在 Kubernetes (k8s) 中，readinessProbe 是一种检查机制，用于确定 Pod 中的容器是否已准备好开始接收流量。当容器启动后，它可能需要一段时间来加载应用程序和数据。readinessProbe 确保流量只有在容器准备好后才会被路由到该容器。如果 readinessProbe 失败，Kubernetes 不会向该 Pod 发送请求。
+
+## ServiceMonitor
+ServiceMonitor 是 Kubernetes 环境中与 Prometheus 监控系统相关的概念，主要用于在 Prometheus Operator 框架下自动发现和配置目标服务的监控。Prometheus Operator 是一个 Kubernetes Operator，用于在 Kubernetes 上更容易地部署和管理 Prometheus 监控实例。
+
 + 确保Kubernetes集群状态良好
 kubectl cluster-info
 
@@ -56,7 +69,6 @@ docker inspect xxx
 # Content Security Policy
 > 如果遇到报错: because it violates the following Content Security Policy directive:
 考虑 ingress 中 add_ header Content-Security-Policy "default-src 'self' *.domain.com
-
 
 
 
