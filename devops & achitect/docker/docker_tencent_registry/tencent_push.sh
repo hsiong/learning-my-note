20bb7a5224c3e55928bf07fccfd7c40c61e0f492
hsiong <peak1203@126.com>
2026-06-23 18:15:37 +0800

    feat: add tencent registry push script
    - add old image tag cleanup logic
    - add docker build and push commands

---
+++ devops & achitect/docker/docker_tencent_registry/tencent_push.sh
@@ -0,0 +1,16 @@
+
+docker login ccr.ccs.tencentyun.com --username="${USERNAME}"
+
+echo "==> 删除本地旧的 ghcr.io/openclaw/openclaw:* tag（排除当前版本 ${VERSION}）"
+docker images "ghcr.io/openclaw/openclaw" --format "{{.Repository}}:{{.Tag}}" \
+| grep -v ":${VERSION}$" \
+| xargs -r docker rmi || true
+
+
+docker images "ccr.ccs.tencentyun.com/openclaw-jf/openclaw" --format "{{.Repository}}:{{.Tag}}" \
+| grep -v ":${VERSION}$" \
+| xargs -r docker rmi || true
+
+docker build -t "${DST_IMAGE_DOCKERCLI}" -f Dockerfile.gateway-dockercli .
+
+docker push "${DST_IMAGE_DOCKERCLI}"
